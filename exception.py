62f75e0760873a2124fc9eed763ea885baaab6fe
mvoin21 <64645177+mvoin21@users.noreply.github.com>
2020-05-11 16:48:49 +0600

    Update exception.py

--- exception.py
+++ exception.py
@@ -1,5 +1,5 @@
 class User:
-    def excpt_decorator(func):
+    def account_decorator(func):
         def wrapper(*args, **kwargs):
             try:
                 print(func(*args, **kwargs))
@@ -7,18 +7,25 @@ class User:
                 raise Exception('No username defined!')
         return wrapper
     
+    def password_decorator(func):
+        def wrapper(*args, **kwargs):
+            try:
+                print(func(*args, **kwargs))
+            except Exception as e:
+                raise Exception('No password to change!')
+        return wrapper
             
-    @excpt_decorator
+    @account_decorator
     def get_account_balance(self, balance):
         self.balance = balance 
         print(self.balance)
     
-    @excpt_decorator
+    @password_decorator
     def  change_password(self, password):
         self.password = password
         print(self.password)
     
 
 u = User()
-# u.get_account_balance()
-# u.change_password()
+u.get_account_balance()
+u.change_password()
